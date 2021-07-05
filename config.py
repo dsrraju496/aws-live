b7ae465409b9243e23847141530bf227f8aade74
sitaram496 <sitaramaraju046@gmail.com>
2021-07-05 14:51:03 +0530

    fixed changes

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employee.cq6n8bva8lqk.us-east-2.rds.amazonaws.com"
-customuser = "admin"
-custompass = "123456789"
+customuser = "ram"
+custompass = "raky"
 customdb = "employee"
 custombucket = "myemployee1234"
 customregion = "us-east-2"
